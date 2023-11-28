fb556263cd2cc7506a9d082a824e231156cd75ad
Yusuf Karabay <yusuf.karabay@hbv.edu.tr>
2023-11-28 06:54:34 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,6 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://gbpdqbpc:R6Wr7sQNfJDEe9_PHWFhxjvC73gYhZlg@fish.rmq.cloudamqp.com/gbpdqbpc");
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
