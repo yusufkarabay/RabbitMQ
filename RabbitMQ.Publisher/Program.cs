b870d25094a92e6a458e983441e5472db54c849e
Yusuf Karabay <yusuf.karabay@hbv.edu.tr>
2023-11-28 07:11:50 +0300

    RM-1:Queue communication tested.

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,6 +1,13 @@
 ﻿using RabbitMQ.Client;
+using System.Text;
 
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://gbpdqbpc:R6Wr7sQNfJDEe9_PHWFhxjvC73gYhZlg@fish.rmq.cloudamqp.com/gbpdqbpc");
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue", true, false, false, null);
+var message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(string.Empty, "demo-queue", null, body);
+Console.WriteLine("Message Sent");
+Console.ReadLine();
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://gbpdqbpc:R6Wr7sQNfJDEe9_PHWFhxjvC73gYhZlg@fish.rmq.cloudamqp.com/gbpdqbpc");
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel); 
+channel.BasicConsume("demo-queue", true, consumer);
+consumer.Received += (sender, e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Mesajınız: "+message);
+};
+Console.ReadLine();
